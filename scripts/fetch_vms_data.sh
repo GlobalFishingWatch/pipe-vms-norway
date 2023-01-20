b58476bfffd55154c1eaa8d1c12c24b1621a1137
Rodrigo Fuentes <rodrigo.fuentes@globalfishingwatch.org>
2023-01-20 09:36:40 -0300

    Use predefined list of downloads when not found

--- scripts/fetch_vms_data.sh
+++ scripts/fetch_vms_data.sh
@@ -62,6 +62,33 @@ fetch_vms_data () {
     sed -r 's#<a href="([^"]+)"[^>]*+*>[^<]+([0-9]{4})[^<]+$#\1#' | \
     # add the domain if the link does not have it
     sed -r 's#(\/.*)#https://www.fiskeridir.no\1#'`
+  
+  if [ -z "$ZIPURL" ]; then
+    # When the file to download is not found in ther downloads page
+    # we'll use the predefined list of files for years 2011-2021 and
+    # a general rule for the files from 2022 and on
+
+    if [[ $(($YEAR)) -ge 2022 ]]; then
+      # Starting in 2022 the files will (hopefully) be published in 
+      # https://register.fiskeridir.no/vms-ers/${YEAR}-VMS.csv.zip
+      ZIPURL="https://register.fiskeridir.no/vms-ers/${YEAR}-VMS.csv.zip"
+    else
+      # Between 2011 and 2021 we should use this predefined list
+      ZIPLIST="2021|https://www.fiskeridir.no/Tall-og-analyse/AApne-data/posisjonsrapportering-vms/_/attachment/download/d8736f20-309c-4b29-9786-a8d8271418c4:300bd8f940c7856c2fbeb4b2053d4fcd989f43e2/posisjonsrapportering-vms-2021-pos.zip
+2020|https://www.fiskeridir.no/Tall-og-analyse/AApne-data/posisjonsrapportering-vms/_/attachment/download/6cba325a-4f84-4002-905a-7f25d4a6cfca:75ca7ee9be41703018c77cfc6ffa61d2d9c89b54/posisjonsrapportering-vms-2020-pos.zip
+2019|https://www.fiskeridir.no/Tall-og-analyse/AApne-data/posisjonsrapportering-vms/_/attachment/download/46c58ae4-ef97-4341-8091-c0e6e2a91398:349529ab7b91aec15b07f395222854257186bdf8/posisjonsrapportering-vms-2019-pos.zip
+2018|https://www.fiskeridir.no/Tall-og-analyse/AApne-data/posisjonsrapportering-vms/_/attachment/download/58039e8f-8b3b-469c-bafa-242f8076fefb:4fd644cf9e8c2aba415a0dc7d6e278d0e6152c39/posisjonsrapportering-vms-2018-pos.zip
+2017|https://www.fiskeridir.no/Tall-og-analyse/AApne-data/posisjonsrapportering-vms/_/attachment/download/150d09d9-1d6c-4168-953d-9266b2aaa120:97ad1c34216cc01af9804fc7221f079b427f0bbc/posisjonsrapportering-vms-2017-pos.zip
+2016|https://www.fiskeridir.no/Tall-og-analyse/AApne-data/posisjonsrapportering-vms/_/attachment/download/fe17a8a2-eed3-43a4-868d-c84699d50953:16922efa37906d3096c174481c884d90d5e3abe1/posisjonsrapportering-vms-2016-pos.zip
+2015|https://www.fiskeridir.no/Tall-og-analyse/AApne-data/posisjonsrapportering-vms/_/attachment/download/a89b811e-76cf-4660-8d27-ee7bf44ecd1c:42441a3d6e044f750019e8c033d2041445211454/posisjonsrapportering-vms-2015-pos.zip
+2014|https://www.fiskeridir.no/Tall-og-analyse/AApne-data/posisjonsrapportering-vms/_/attachment/download/45ce7608-5b72-4b61-9894-73a5d7ebe77c:b0fd74ccc40eefad69ba3c30a59e296ac00bca42/posisjonsrapportering-vms-2014-pos.zip
+2013|https://www.fiskeridir.no/Tall-og-analyse/AApne-data/posisjonsrapportering-vms/_/attachment/download/d963aac6-8b02-4c21-a694-ce506bd735c0:cf0e61502e0f38728e7fb81508551f9cf0023036/posisjonsrapportering-vms-2013-pos.zip
+2012|https://www.fiskeridir.no/Tall-og-analyse/AApne-data/posisjonsrapportering-vms/_/attachment/download/f51512f5-5387-4311-955a-4d27503989bf:bcb1aa871de370dbbd7be50851f31d308d1e4015/posisjonsrapportering-vms-2012-pos.zip
+2011|https://www.fiskeridir.no/Tall-og-analyse/AApne-data/posisjonsrapportering-vms/_/attachment/download/a6d73f29-21ad-44a1-b328-aec19fd62744:9d2f30adddd2bf8f2eb14e15e480df8df3c2415a/posisjonsrapportering-vms-2011-pos.zip"
+      ZIPURL=`echo "$ZIPLIST" | | grep -Po "(?<=${YEAR}\|).*"`
+    fi
+  fi
+
   if [ -z "$ZIPURL" ]; then
     echo "Could not find the positions report csv file to download for year ${YEAR} on ${REPORTSINDEXURL}."
     return 1
