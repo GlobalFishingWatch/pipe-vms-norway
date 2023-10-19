9be110426de19f8aed18d40142bb98eadcc45a8e
Matias Piano <matias@globalfishingwatch.org>
2023-10-19 13:41:58 -0300

    Updates project version to following tag version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 setup(
-    version='v3.1.1',
+    version='v3.3.2',
     author='engineering@globalfishingwatch.org',
     packages=find_packages(exclude=['test*.*', 'tests'])
 )
