be02c0d973fa18923731f9dddb195aa8cd832c7f
Rodrigo Fuentes <rodrigo.fuentes@globalfishingwatch.org>
2024-01-05 14:12:04 -0300

    bump patch version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 setup(
-    version='v3.3.2',
+    version='v3.3.3',
     author='engineering@globalfishingwatch.org',
     packages=find_packages(exclude=['test*.*', 'tests'])
 )
