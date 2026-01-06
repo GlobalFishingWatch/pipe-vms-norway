6e4e75412e44a848291c792d9047dd65117eaacb
Rodrigo Fuentes <rodrigo.fuentes@globalfishingwatch.org>
2026-01-06 10:32:04 -0300

    Bump version to v3.3.10 in setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python
 
-from setuptools import find_packages
-from setuptools import setup
+from setuptools import find_packages, setup
 
 setup(
-    version='v3.3.9',
+    version='v3.3.10',
     author='engineering@globalfishingwatch.org',
     packages=find_packages(exclude=['test*.*', 'tests'])
 )
